54a2d0daaee331fe46f6a476d3dc49fc23033d9c
Maggie Ronan <margaret.ronan@gmail.com>
2020-03-25 13:39:44 -0700

    Updates

---
+++ precache-manifest.adeb30ca77fec919a5250b5e2dda01bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3304904f1c914a1a9f6461f982fbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3509f87816d16eb77b6b",
+    "url": "/static/css/main.8a4ab049.chunk.css"
+  },
+  {
+    "revision": "cdb9e92559d02726787d",
+    "url": "/static/js/2.8b755791.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.8b755791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3509f87816d16eb77b6b",
+    "url": "/static/js/main.3544b7fb.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "49265a1459058513b3d59371241d9c46",
+    "url": "/static/media/profile_photo.49265a14.jpeg"
+  }
+]);
